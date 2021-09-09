3da1b0669ec5232dbab9f509a755a5415b70a3f7
Giovanne Afonso <giovanneafonso@gmail.com>
2021-09-08 23:25:48 -0300

    Champion list

--- wordpress/themes/generatepress-child/functions.php
+++ wordpress/themes/generatepress-child/functions.php
@@ -1,13 +1,20 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/lib/TwigTemplates.php';
+define('CHILDTHEME_BASE_DIR', __DIR__);
+
+require_once CHILDTHEME_BASE_DIR . '/vendor/autoload.php';
+require_once CHILDTHEME_BASE_DIR . '/lib/TwigTemplates.php';
 
 // Load templates
-TwigTemplates::init(__DIR__ . '/templates');
+TwigTemplates::init(CHILDTHEME_BASE_DIR . '/templates');
 
 function shortcode_lol_champions_list ($attrs) {
-  return TwigTemplates::render('lol_champions_list', ['attrs' => $attrs]);
+  $champions = json_decode(file_get_contents(CHILDTHEME_BASE_DIR . '/assets/json/champions.json'), true);
+
+  return TwigTemplates::render('lol_champions_list', [
+    'attrs' => $attrs,
+    'champions' => $champions,
+  ]);
 }
 
 add_shortcode('lol_champions_list', 'shortcode_lol_champions_list');
